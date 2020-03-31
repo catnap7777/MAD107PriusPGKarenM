0aafe542fe63534e77e1dd988b6876f654b9b470
km <35552602+catnap7777@users.noreply.github.com>
2020-03-30 21:41:33 -0500

    initial commit

--- MAD107PriusPGKarenM.playground/Contents.swift
+++ MAD107PriusPGKarenM.playground/Contents.swift
@@ -38,6 +38,46 @@ var priusModelDictionary = ["L Eco": (price: 24,325),
                             "XLE AWD-e": (price: 29,375)
 ]
 
+enum CarColor: String {
+    case Blue = "Electric Storm Blue"
+    case Pearl = "Blizzard Pearl"
+    case Silver = "Classic Silver Metallic"
+    case Gray = "Magnetic Gray Metallic"
+    case Black = "Midnight Black Metallic"
+    case Red = "Supersonic Red"
+    case Sea = "Sea Glass Pearl"
+}
+
+enum ExtAcc: String {
+    case SpokeAW = "15in 10-Spoke Alloy Wheels"
+    case AeroSS = "Aero Side Splitter"
+    case AlloyWL = "Alloy Wheel Locks"
+    case BlackoutWI = "Blackout Wheel Inserts"
+    case BodySM = "Body Side Molding"
+    case DoorEG = "Door Edge Guards"
+    case PaintPF = "Paint Protect Film"
+    case RearBA = "Rear Bumper Applique"
+    case RearBP = "Rear Bumper Protector"
+    case RemovableCB = "Removable Cross Bars"
+}
+
+enum IntAcc: String {
+    case AllWFL = "All Weather Floor Liners"
+    case CargoN = "Cargo Net - Envelope"
+    case CargoT = "Cargo Tote"
+    case CargoL = "Cargo Liner"
+    case CarpetCM = "Carpet Cargo Mat"
+    case CarpetFM = "Carpet Floor Mats"
+    case CoinHAC = "Coin Holder/Ashtray Cup"
+    case EmergK = "Emergency Kit"
+    case FirstAK = "First Aid Kit"
+    case FlessHM = "Frameless Homelink Mirror"
+    case IllumDS = "Illuminated Door Sills"
+    case SecurS = "Security System"
+    case UnivTH = "Universal Table Holder"
+    
+}
+
 let taxRateE = 0.075
 
 var tipCalculatedE: Double = 0
